df35a3ee15ce37aa365f7c872fe5e780897672e0
Jørgen Lund <jlu@dips.no>
2023-08-15 10:52:24 +0200

    Add Label Studio setup

--- convert.py
+++ convert.py
@@ -11,7 +11,7 @@ class Arguments(Tap):
     """The JSON file with annotated notes to convert to a specific format."""
     output: pathlib.Path = 'results.csv'
     """The filename or directory to write converted documents to."""
-    format: Literal['csv', 'xml'] = 'csv'
+    format: Literal['csv', 'xml', 'labelstudio'] = 'csv'
     """The format to convert notes to."""
 
 def main(args: Arguments):
@@ -22,6 +22,8 @@ def main(args: Arguments):
         create_csv(source, args.output, args)
     elif args.format == 'xml':
         create_xml(source, args.output, args)
+    elif args.format == 'labelstudio':
+        create_labelstudio(source, args.output, args)
     else:
         raise ValueError(f"Unrecognized format '{args.format}'")
 
@@ -43,6 +45,11 @@ def create_xml(source, output_path: pathlib.Path, args: Arguments):
             output.write(r)
             output.write(f"</record>")
 
+def create_labelstudio(source, output_path: pathlib.Path, args: Arguments):
+    results = source['results']
+    tasks = [{'id': i, 'data': {'text': utilities.tags.remove_tags(r), 'original_text': r}, } for i, r in enumerate(results)]
+    with open(output_path, 'w', encoding='utf8') as output:
+        json.dump(tasks, output)
 
 if __name__ == '__main__':
     args = Arguments()
