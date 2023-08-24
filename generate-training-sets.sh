ac13c5fe7ecbec8e05b63696f7ceda4d2e62fd8b
Jørgen Lund <jlu@dips.no>
2023-08-24 12:44:52 +0200

    Divide vocabularies into training and holdout sets

---
+++ generate-training-sets.sh
@@ -0,0 +1,5 @@
+python split-train-holdout.py --holdout_size 0.1 --input vocabularies/all/en_diagnoses.csv --training vocabularies/training/en_diagnoses.csv --holdout vocabularies/holdout/en_diagnoses.csv --seed 18994 --verbose
+python split-train-holdout.py --holdout_size 0.1 --input vocabularies/all/nb_cities.csv --training vocabularies/training/nb_cities.csv --holdout vocabularies/holdout/nb_cities.csv --seed 18995 --verbose
+python split-train-holdout.py --holdout_size 0.1 --input vocabularies/all/nb_family_names.csv --training vocabularies/training/nb_family_names.csv --holdout vocabularies/holdout/nb_family_names.csv --seed 18996 --verbose
+python split-train-holdout.py --holdout_size 0.1 --input vocabularies/all/nb_given_names.csv --training vocabularies/training/nb_given_names.csv --holdout vocabularies/holdout/nb_given_names.csv --seed 18997 --verbose
+python split-train-holdout.py --holdout_size 0.1 --input vocabularies/all/nb_healthcare_units.csv --training vocabularies/training/nb_healthcare_units.csv --holdout vocabularies/holdout/nb_healthcare_units.csv --seed 18998 --verbose
